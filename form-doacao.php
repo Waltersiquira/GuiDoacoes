4c0ff6126b2b4c20fef131247589fcf0505b603f
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-06 11:32:42 -0300

    Create form-doacao.php

---
+++ form-doacao.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiDoacões</title>
+</head>
+<body>
+    <form action="enviar-form-doacao.php" method="get">
+        digite o nome da doação <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição da doação <br>
+        <input type="text" name="descricao"> <br>
+        digite o preço da doação <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem da doação <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
