87523f51104ef858b3de74162038fc5415f2bbfb
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-06 11:49:46 -0300

    Update doar.php

--- doar.php
+++ doar.php
@@ -6,8 +6,14 @@
     <title>GuiDoacões</title>
 </head>
 <body style="text-align: center;">
-    <h1>Aprovado</h1>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Approved_img.png" alt="">
-    <p>Doação feita com sucesso</p>
+    <?php 
+    require_once 'includes/lojas.php';
+    $id2 = $_GET['i'] ?? 1;
+    if ($l->query("UPDATE doacoes SET totdoacoes = totdoacoes + 1 WHERE id = $id2") == true){
+        echo "<h1>Aprovado</h1><img src='https://upload.wikimedia.org/wikipedia/commons/8/84/Approved_img.png' alt=''><p>Doação feita com sucesso</p>";
+    } else {
+        echo 'error';
+    }
+    ?>
 </body>
 </html>
